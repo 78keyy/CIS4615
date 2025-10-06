2a76f4a2d3b261ed024df26f34af29cd137bd069
Keysari <147214339+78keyy@users.noreply.github.com>
2025-10-05 22:53:59 -0400

    Update R04_STR03_J.java

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -2,17 +2,16 @@ import java.math.BigInteger;
 
 public class R04_STR03_J {
     
-    // Non-compliant code
     public void processBigIntegerNonCompliant() {
         BigInteger x = new BigInteger("530500452766");
         byte[] byteArray = x.toByteArray();
-        String s = new String(byteArray); // Incorrect conversion
+        String s = new String(byteArray); 
         byteArray = s.getBytes();
         x = new BigInteger(byteArray);
         System.out.println("Non-compliant result: " + x);
     }
     
-    // Compliant solution
+
     public void processBigIntegerCompliant() {
         BigInteger x = new BigInteger("530500452766");
         String s = x.toString(); // Valid character data
