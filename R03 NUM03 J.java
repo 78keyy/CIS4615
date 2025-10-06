26c74080ced19fca6487a14d44ddd5d1498b70a1
Keysari <147214339+78keyy@users.noreply.github.com>
2025-10-05 22:59:18 -0400

    Update R03 NUM03 J.java

--- R03 NUM03 J.java
+++ R03 NUM03 J.java
@@ -4,12 +4,12 @@ import java.io.ByteArrayInputStream;
 
 public class R03_NUM03_J {
     
-    //non-compliant code
+  
     public static int getIntegerNonCompliant(DataInputStream is) throws IOException {
         return is.readInt();
     }
     
-    //compliant solution
+
     public static long getIntegerCompliant(DataInputStream is) throws IOException {
         return is.readInt() & 0xFFFFFFFFL;
     }
@@ -20,11 +20,10 @@ public class R03_NUM03_J {
             byte[] data = {0x12, 0x34, 0x56, 0x78};
             DataInputStream dis = new DataInputStream(new ByteArrayInputStream(data));
             
-            // Non-compliant usage
             int badResult = getIntegerNonCompliant(dis);
             System.out.println("Non-compliant result: " + badResult);
             
-            // Reset stream for compliant usage
+       
             dis = new DataInputStream(new ByteArrayInputStream(data));
             long goodResult = getIntegerCompliant(dis);
             System.out.println("Compliant result: " + goodResult);
