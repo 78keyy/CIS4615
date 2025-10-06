2e595e509b3b8e04a8103d995de48e356ea5c617
Keysari <147214339+78keyy@users.noreply.github.com>
2025-10-05 22:56:25 -0400

    Update R02 XP00 J.java

--- R02 XP00 J.java
+++ R02 XP00 J.java
@@ -2,14 +2,14 @@ import java.io.File;
 
 public class R02_XP00_J {
     
-    // Non-compliant code
+   
     public void deleteFileNonCompliant() {
         File someFile = new File("someFileName.txt");
         // Do something with someFile
         someFile.delete(); // Return value ignored
     }
     
-    // Compliant solution
+
     public void deleteFileCompliant() {
         File someFile = new File("someFileName.txt");
         // Do something with someFile
